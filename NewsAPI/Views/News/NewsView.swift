f6a00d0070d41d1b825c0315d5b2ffc19b43e423
OlehOliinykov <oleh38568@gmail.com>
2023-10-05 13:22:48 +0300

    Added News views

--- NewsAPI/Views/News/NewsView.swift
+++ NewsAPI/Views/News/NewsView.swift
@@ -8,19 +8,127 @@
 import SwiftUI
 
 struct NewsView: View {
+    private enum Constants {
+        enum NavigationTitle {
+            static let navigationDetailTitle = "Detail"
+        }
+        
+        enum CornerRadius {
+            static let defaultCornerRadius: CGFloat = 8
+        }
+    }
+    
     @ObservedObject var viewModel: NewsViewModel
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollViewReader { proxy in
+                ScrollView {
+                    VStack {
+                        HStack {
+                            searchField
+                            searchButton
+                        }
+                        Divider()
+                        resultView
+                    }
+                }
+            }
+            .sheet(isPresented: $viewModel.showFiltersSheet, content: {
+                FiltersSheetView(startDate: $viewModel.startDate,
+                                 endDate: $viewModel.endDate, languages: $viewModel.language,
+                                 sortingParameter: $viewModel.sortingParameter,
+                                 pageSize: $viewModel.pageSize, 
+                                 searchWordIn: $viewModel.searchIn)
+            })
+            .toolbar {
+                Button {
+                    viewModel.showFiltersSheet.toggle()
+                } label: {
+                    Image(systemName: "line.3.horizontal.decrease.circle")
+                }
+            }
+            .navigationTitle("News")
+            .alert(viewModel.alertMessage, isPresented: $viewModel.showAlert) {
+                Button("OK", role: .cancel) { }
+            }
         }
-        .padding()
+    }
+    
+    private var searchField: some View {
+        TextField("Search", text: $viewModel.searchText)
+            .textFieldStyle(.roundedBorder)
+            .padding(.leading)
+    }
+    
+    private var searchButton: some View {
+        Button("Search") {
+            viewModel.news.removeAll()
+            viewModel.findNews()
+        }
+        .foregroundColor(.white)
+        .padding(7)
+        .background(.blue)
+        .cornerRadius(Constants.CornerRadius.defaultCornerRadius)
+        .padding(.trailing)
+    }
+    
+    @ViewBuilder
+    private var resultView: some View {
+        switch viewModel.newsState {
+        case .appStart:
+            startMessage
+        case .emptyList:
+            emptyList
+        case .loaded:
+            newsList
+        case .loading:
+            progressView
+        }
+    }
+    
+    private var startMessage: some View {
+        VStack {
+            Spacer()
+            Text("Let`s find some news")
+                .multilineTextAlignment(.center)
+            Spacer()
+        }
+    }
+    
+    private var emptyList: some View {
+        VStack {
+            Spacer()
+            Text("Oops...\nlooks like we couldn't find anything")
+                .multilineTextAlignment(.center)
+            Spacer()
+        }
+    }
+    
+    private var newsList: some View {
+        VStack{
+            ForEach(viewModel.news, id: \.self) { news in
+                NavigationLink {
+                    NewsDetailView(articles: news)
+                } label: {
+                    NewsCell(article: news)
+                }
+            }
+        }
+    }
+    
+    private var progressView: some View {
+        VStack {
+            Spacer()
+            ProgressView {
+                Text("Trying find something for you...")
+            }
+            Spacer()
+        }
     }
 }
 
 #Preview {
     NewsView(viewModel: NewsViewModel())
 }
+
--- NewsAPI/Views/News/NewsViewModel.swift
+++ NewsAPI/Views/News/NewsViewModel.swift
@@ -6,7 +6,71 @@
 //
 
 import Foundation
+import Combine
+
+enum NewsSearchStates: CaseIterable {
+    case appStart
+    case emptyList
+    case loaded
+    case loading
+}
 
 final class NewsViewModel: ObservableObject {
+    @Published var newsState: NewsSearchStates = .appStart
     
+    @Published var news = [Articles]()
+    
+    @Published var searchText: String = ""
+    
+    @Published var currentPage: Int = 1
+    @Published var language: Languages = .en
+    @Published var pageSize: PageSize = .small
+    @Published var searchIn: SearchWordIn = .title
+    @Published var sortingParameter: SortingParameters = .publishedAt
+    @Published var startDate: Date = (Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date())
+    @Published var endDate: Date = .now
+    
+    @Published var showFiltersSheet: Bool = false
+    
+    @Published var showAlert: Bool = false
+    @Published var alertMessage: String = ""
+    
+    @Published var offset: Int = 0
+    @Published var isLoadedAll: Bool = false
+    
+    private var totalResults: Int = 0
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    func findNews() {
+        newsState = .loading
+        NewsService.findNewsPublisher(searchText,
+                                      languages: language,
+                                      searchIn: searchIn,
+                                      sortParam: sortingParameter,
+                                      from: TimeFormatter.searchDateFormatter(date: startDate),
+                                      to: TimeFormatter.searchDateFormatter(date: endDate), 
+                                      page: String(currentPage),
+                                      pageSize: pageSize)
+        .receive(on: DispatchQueue.global(qos: .background))
+        .sink { [weak self] result in
+            switch result {
+            case .finished:
+                break
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self?.showAlert = true
+                    self?.alertMessage = error.localizedDescription
+                }
+            }
+        } receiveValue: { [weak self] response in
+            guard let self else { return }
+            DispatchQueue.main.async {
+                self.news = response.articles
+                print("djsdfjsdj: \(self.news.count)")
+                self.newsState = self.news.isEmpty ? .emptyList : .loaded
+            }
+        }
+        .store(in: &cancellables)
+    }
 }
