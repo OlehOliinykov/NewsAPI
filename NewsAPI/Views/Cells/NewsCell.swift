1cb2e11748235316cf33d7afd1271a797f1bd5c0
OlehOliinykov <oleh38568@gmail.com>
2023-10-05 13:24:24 +0300

    Added news cell and detail news view

---
+++ NewsAPI/Views/Cells/NewsCell.swift
@@ -0,0 +1,62 @@
+//
+//  NewsCell.swift
+//  NewsAPI
+//
+//  Created by Oleh Oliinykov on 03.10.2023.
+//
+
+import SwiftUI
+
+struct NewsCell: View {
+    private enum Constants {
+        enum CornerRadius {
+            static let defaultCornerRadius: CGFloat = 8
+        }
+        
+        enum LineLimits {
+            static let defaultLineLimit: Int = 3
+        }
+    }
+    
+    let article: Articles
+    var body: some View {
+        LazyVStack {
+            title
+            description
+        }
+        .background(.gray.opacity(0.05))
+        .cornerRadius(Constants.CornerRadius.defaultCornerRadius)
+        .padding()
+    }
+    
+    private var title: some View {
+        HStack {
+            if let title = article.title {
+                Text(title)
+                    .foregroundColor(.black)
+                    .bold()
+                    .padding()
+                    .multilineTextAlignment(.leading)
+            }
+            Spacer()
+        }
+    }
+    
+    private var description: some View {
+        HStack {
+            if let description = article.description {
+                Text(description)
+                    .font(.body)
+                    .foregroundColor(.gray)
+                    .lineLimit(3)
+                    .padding()
+                    .multilineTextAlignment(.leading)
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    NewsCell(article: .init(source: Source(id: "id", name: "name"), author: "Author", title: "NHC tracking Tropical Storm Bret, Tropical Depression 4. Forecast path - Palm Beach Post", description: "Tropical Depression Four is expected to become a tropical storm in the next day or so. Next name for the season is Cindy.", urlToImage: URL(string: "URL"), publishedAt: "kolis`!"))
+}
---
+++ NewsAPI/Views/Details/NewsDetailView.swift
@@ -0,0 +1,113 @@
+//
+//  NewsDetailView.swift
+//  NewsAPI
+//
+//  Created by Oleh Oliinykov on 03.10.2023.
+//
+
+import SwiftUI
+
+
+
+struct NewsDetailView: View {
+    private enum Constants {
+        enum NavigationTitle {
+            static let navigationDetailTitle = "Detail"
+        }
+        
+        enum CornerRadius {
+            static let defaultCornerRadius: CGFloat = 8
+        }
+    }
+    
+    var articles: Articles
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 16) {
+                NewsImage
+                NewsDate
+                NewsSourceName
+                NewsTitle
+                NewsDescription
+                NewsAuthor
+            }
+        }
+        .navigationTitle(Constants.NavigationTitle.navigationDetailTitle)
+        .navigationBarTitleDisplayMode(.large)
+    }
+    
+    @ViewBuilder
+    private var NewsImage: some View {
+        if let imageUrl = articles.urlToImage {
+            AsyncImage(url: imageUrl) { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(Constants.CornerRadius.defaultCornerRadius)
+            } placeholder: {
+                ProgressView()
+                    .padding()
+            }
+            .padding(.horizontal)
+        }
+    }
+    
+    @ViewBuilder
+    private var NewsDate: some View {
+        if let date = TimeFormatter.dateFormatter(date: articles.publishedAt) {
+            Text(date)
+        }
+    }
+    
+    @ViewBuilder
+    private var NewsSourceName: some View {
+        HStack {
+            if let name = articles.source.name {
+                Text(name)
+                    .padding(.horizontal)
+            }
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    private var NewsTitle: some View {
+        HStack {
+            if let title = articles.title {
+                Text(title)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                    .padding(.horizontal)
+            }
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    private var NewsDescription: some View {
+        if let description = articles.description {
+            Text(description)
+                .font(.body)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.leading)
+                .padding(.horizontal)
+        }
+    }
+    
+    @ViewBuilder
+    private var NewsAuthor: some View {
+        if let author = articles.author {
+            HStack {
+                Spacer()
+                Text(author)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct NewsDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsDetailView(articles: .init(source: Source(id: "id", name: "CBS Sports"), author: "Tracey Harrington McCoy", title: "NHC tracking Tropical Storm Bret, Tropical Depression 4. Forecast path - Palm Beach Post", description: "Tropical Depression Four is expected to become a tropical storm in the next day or so. Next name for the season is Cindy.", urlToImage: URL(string: "URL"), publishedAt: "2023-06-21T21:02:00Z"))
+    }
+}
